4edc7edd12ac8e4787458aeedd4c50520cca9e3e
Artem S. <artyom.sol23@icloud.com>
2025-06-29 13:47:14 +0300

    Update TF-IDF.py

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,19 +1,15 @@
 from math import log10
-from collections import defaultdict
 
-doc1 = "My dog is on my table .".lower().split()
-doc2 = "The dog is on the table .".lower().split()
-docs = [doc1, doc2]
-
-target_word = "my"
-
-term_count = doc1.count(target_word)
-tf = term_count / len(doc1)
-
-doc_count = sum(1 for doc in docs if target_word in doc)
-idf = log10(len(docs) / doc_count)
-
-tf_idf = tf * idf
-tf_idf_rounded = round(tf_idf, 1)
-
-print(tf_idf_rounded)
+def calculate_tfidf(target_word: str, docs: list):
+  if not docs:
+        return 0.0
+    
+    term_count = docs[0].count(target_word)
+    tf = term_count / len(docs[0]) if docs[0] else 0.0
+    
+    doc_count = sum(1 for doc in docs if target_word in doc)
+    idf = log10(len(docs) / doc_count) if doc_count > 0 else 0.0
+    
+    tf_idf = tf * idf
+    
+    return round(tf_idf, 1)
